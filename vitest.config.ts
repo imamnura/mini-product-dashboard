571f7c2aa1149b89f51e18e93be7c4be409febc0
imamnura <imam.12ra.kkpi@gmail.com>
2025-10-24 00:00:46 +0700

    config unit test vitest

---
+++ vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vitest/config";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+    coverage: { provider: "v8", reporter: ["text", "json", "html"] },
+  },
+});
