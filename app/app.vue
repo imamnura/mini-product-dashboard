35bf792527f0abf044b0b7cedd5a8399e6b4cdf9
imamnura <imam.12ra.kkpi@gmail.com>
2025-10-23 09:34:10 +0700

    initial nuxt

---
+++ app/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="min-h-screen bg-white text-zinc-900 dark:bg-zinc-950 dark:text-zinc-100"
+  >
+    <LayoutNavbar>
+      <template #search>
+        <UiSearchBar v-model="q" @change="onSearch" />
+      </template>
+    </LayoutNavbar>
+
+    <NuxtPage :page-key="route.fullPath" />
+
+    <footer
+      class="border-t mt-6 py-6 text-xs text-zinc-500 dark:border-zinc-800"
+    >
+      <div class="container flex items-center justify-between">
+        <p>© 2025 Orange Store. All rights reserved.</p>
+        <nav class="flex gap-6">
+          <NuxtLink to="/">Privacy Policy</NuxtLink>
+          <NuxtLink to="/">Terms of Service</NuxtLink>
+          <NuxtLink to="/">Contact Us</NuxtLink>
+        </nav>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+const qState = useState("global:q", () => "");
+const q = ref(qState.value);
+function onSearch(v: string) {
+  qState.value = v;
+}
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  css: ["~/assets/css/tailwind.css"],
+  app: {
+    head: {
+      title: "Orange Store",
+      meta: [
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content: "Simple product dashboard using FakeStore API",
+        },
+      ],
+      link: [{ rel: "preconnect", href: "https://fakestoreapi.com" }],
+    },
+  },
+  postcss: {
+    plugins: {
+      "@tailwindcss/postcss": {},
+      autoprefixer: {},
+    },
+  },
+  routeRules: {
+    "/": { ssr: true, swr: 60 }, // buat cache 60 detik
+    "/category/**": { ssr: true, swr: 60 },
+    "/products/**": { ssr: true, swr: 60 },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: { API_BASE: "https://fakestoreapi.com" },
+  },
+  nitro: { preset: "node" },
+});
